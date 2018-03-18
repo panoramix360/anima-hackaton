52f99302290d3da0a79546098afff36f8286c2c5
Lucas Reis <lucas.reis@MacBook-Air-de-Radix.local>
2018-03-18 10:38:14 -0300

    Início do consolidado de aulas

--- backend/src/business/src/aula-business.js
+++ backend/src/business/src/aula-business.js
@@ -5,16 +5,25 @@
  */
 
 import {
-  Aula
+  Aula,
+  Presenca
 } from '../../entity';
 import {
   DummyFilter
 } from '../../entity/filter';
+import { groupBy } from 'lodash';
 import CrudLoggerBusiness from '../../lib/crud-logger-business';
 
 export class AulaBusiness extends CrudLoggerBusiness {
+
+  async listAulasDoAluno(alunoId) {
+    var presencaList = await Presenca.find((presenca) => { console.log(presenca) });
+    console.log(presencaList);
+    //presencaList = groupBy(presencaList, (presenca) => presenca.disciplina._id);
+    return presencaList;
+  }
+
   toFilter() {
-    this.logger.trace('Aula');
     return () => true;
   }
 }
--- backend/src/rest/routes/index.js
+++ backend/src/rest/routes/index.js
@@ -16,9 +16,12 @@ import {
 const qrcodeRouter = rdx.http.router();
 qrcodeRouter.get("/gerar/{id}", (req, res, next) => qrcodeBusiness.gerarQrCodeToken(req.params.id).then(token => res.json({token})).catch(next));
 
+const aulaRouter = rdx.http.router();
+aulaRouter.get("/consolidado/:id", (req, res, next) => aulaBusiness.listAulasDoAluno(req.params.id).then(list => res.json(list)).catch(next));
+
 export default {
   "/aluno": rdx.pouchdb.crud.crudRouter(rdx.http.router(), alunoBusiness),
-  "/aula": rdx.pouchdb.crud.crudRouter(rdx.http.router(), aulaBusiness),
+  "/aula": rdx.pouchdb.crud.crudRouter(aulaRouter, aulaBusiness),
   "/disciplina": rdx.pouchdb.crud.crudRouter(rdx.http.router(), disciplinaBusiness),
   "/presenca": rdx.pouchdb.crud.crudRouter(rdx.http.router(), presencaBusiness),
   "/professor": rdx.pouchdb.crud.crudRouter(rdx.http.router(), professorBusiness),
