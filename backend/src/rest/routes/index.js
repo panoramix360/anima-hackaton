c53a476fb559e08cec9e1cfc6f2749d64f4553b2
Fausto Ferreira Junqueira <fausto.junqueira@radixeng.com.br>
2018-03-18 15:21:36 -0300

    judna

--- backend/src/rest/routes/index.js
+++ backend/src/rest/routes/index.js
@@ -17,9 +17,9 @@ import {
 const avaliacaoRouter = rdx.http.router();
 const qrcodeRouter = rdx.http.router();
 
+qrcodeRouter.get("/ultimo", (req, res) => res.json(qrcodeBusiness.ultimo()));
 qrcodeRouter.get("/gerar/:id", (req, res, next) => qrcodeBusiness.gerarQrCodeToken(req.params.id).then(token => res.json({token})).catch(next));
 qrcodeRouter.post("/validar", (req, res, next) => qrcodeBusiness.validarQrCodeToken(req.body.token, req.body.aluno).then(d => res.json(d)).catch(next));
-qrcodeRouter.post("/ultimo", (req, res) => res.json(qrcodeBusiness.ultimo()));
 avaliacaoRouter.post('/avaliar', (req, res, next) => avaliacaoBusiness.avaliar(req.body.presencaId, req.body.estrelas, req.body.descricao).then(d => res.json(d)).catch(next));
 
 const aulaRouter = rdx.http.router();
